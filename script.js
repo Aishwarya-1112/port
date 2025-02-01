f7dddbc21feb124ee4bb142adbc14611e2ed6e10
Aishwarya1155 <aishwaryamanoharann@gmail.com>
2025-02-01 21:50:03 +0530

    toggle final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const menuItems = document.querySelector(".nav-bar"); // Select all <a> tags in 
 const burgermenu = document.querySelector(".burger-menu");
 burgermenu.addEventListener("click", () => {
   if (menuItems.style.display == "none") {
-    menuItems.style.display = "block";
+    menuItems.style.display = "flex";
   } else {
     menuItems.style.display = "none";
   }
